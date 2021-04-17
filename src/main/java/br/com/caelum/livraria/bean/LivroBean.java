04546fc41ff1cebe044a04d0ec2b3b97c5a5bac8
Davi <davipbl@gmail.com>
2021-04-17 11:52:24 -0300

    Tentativa de push do projeto maven

---
+++ src/main/java/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,134 @@
+package main.java.br.com.caelum.livraria.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import main.java.br.com.caelum.livraria.dao.AutorDao;
+import main.java.br.com.caelum.livraria.dao.LivroDao;
+import main.java.br.com.caelum.livraria.modelo.Autor;
+import main.java.br.com.caelum.livraria.modelo.Livro;
+import main.java.br.com.caelum.livraria.tx.Transacional;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+@Named
+@ViewScoped
+public class LivroBean implements Serializable {
+
+	private Livro livro = new Livro();
+	private Integer autorId;
+	private Integer livroId;
+	private List<Livro> livros;
+
+	@Inject
+	LivroDao livroDao;
+
+	@Inject
+	AutorDao autorDao;
+
+	@Inject
+	FacesContext context;
+
+	public Integer getLivroId() {
+		return livroId;
+	}
+
+	public void setLivroId(Integer livroId) {
+		this.livroId = livroId;
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public void carregarLivroPelaId() {
+		this.livro = livroDao.buscaPorId(livroId);
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public List<Livro> getLivros() {
+		if(this.livros == null) {
+		this.livros = livroDao.listaTodos();
+		}
+		return livros;
+	}
+
+	public List<Autor> getAutores() {
+		return autorDao.listaTodos();
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	public void gravarAutor() {
+		Autor autor = autorDao.buscaPorId(this.autorId); //Função utilizada no livro.xhtml para gravar no backend
+		this.livro.adicionaAutor(autor);
+		System.out.println("Livro escrito por: " + autor.getNome());
+	}
+
+	@Transacional
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			context.addMessage("autor",
+					new FacesMessage("Livro deve ter pelo menos um Autor."));
+			this.livro = new Livro();
+			return;
+		}
+
+		if (this.livro.getId() == null) {
+			livroDao.adiciona(this.livro);
+			this.livros = livroDao.listaTodos();
+		} else {
+			livroDao.atualiza(this.livro);
+		}
+
+		this.livro = new Livro();
+	}
+
+	@Transacional
+	public void remover(Livro livro) {
+		System.out.println("Removendo livro");
+		livroDao.remove(livro);
+	}
+
+	public void removerAutorDoLivro(Autor autor) {
+		this.livro.removeAutor(autor);
+	}
+
+	public void carregar(Livro livro) {
+		System.out.println("Carregando livro: " + this.livro.getTitulo());
+		this.livro = this.livroDao.buscaPorId(livro.getId());
+	}
+
+	public String formAutor() {
+		System.out.println("Chamando o formulário do Autor");
+		return "autor?faces-redirect=true";
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+		String valor = value.toString();
+		if(!valor.startsWith("1")) {
+			System.out.println("comecaComDigitoUm validation exception throws:");
+			throw new ValidatorException(new FacesMessage("ISBN deveria começar com 1"));
+		}
+	}
+
+}
---
+++ src/main/java/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,48 @@
+package main.java.br.com.caelum.livraria.bean;
+
+import main.java.br.com.caelum.livraria.dao.UsuarioDao;
+import main.java.br.com.caelum.livraria.modelo.Usuario;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class LoginBean implements Serializable {
+
+	private Usuario usuario = new Usuario();
+
+	@Inject
+	UsuarioDao dao;
+
+	@Inject
+	FacesContext context;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public String efetuaLogin() {
+		System.out.println("fazendo login do usuário " + this.usuario.getEmail());
+
+		boolean existe = dao.existe(this.usuario);
+		if(existe) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			return "livro?faces-redirect=true";
+		}
+
+		context.getExternalContext().getFlash().setKeepMessages(true); //mantém a mensagem gerada na requisição atual para a próxima requisição
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+
+		return "login?faces-redirect=true";
+	}
+
+	public String deslogar() {
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
+	}
+}
